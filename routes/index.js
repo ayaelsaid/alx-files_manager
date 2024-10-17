22135756cc30978bb42c871d68dceab059685a9a
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-17 05:35:26 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
+
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
@@ -10,7 +11,7 @@ router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UserController.getMe);
+router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
