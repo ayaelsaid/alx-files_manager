44f57a53eaf9ccedaffe557ae7125abf81b464a6
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-17 04:52:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,8 @@ import express from 'express';
 import router from './routes/index.js';
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/', router);
 app.listen(port);
