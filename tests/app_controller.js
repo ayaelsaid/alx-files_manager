2dbda58e4328a8d040d0d0de958bfb7135586910
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-17 05:53:32 +0300

    Update app_controller.js

--- tests/app_controller.js
+++ tests/app_controller.js
@@ -1,17 +1,19 @@
 import request from 'supertest';
 import app from '../server';
 import dbClient from '../utils/db';
+import sinon from 'sinon';
 
-describe('GET /status', () => {
+describe('get /status', () => {
   it('should return status 200 and a message', async () => {
+    expect.assertions(2);
     const response = await request(app).get('/status');
 
-    expect(response.status).to.equal(200);
-    expect(response.body).to.deep.equal({ redis: true, db: true });
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ redis: true, db: true });
   });
 });
 
-describe('GET /stats', () => {
+describe('get /stats', () => {
   let nbUsersStub;
   let nbFilesStub;
 
@@ -26,12 +28,13 @@ describe('GET /stats', () => {
   });
 
   it('should return status 200 and user and file counts', async () => {
+    expect.assertions(2);
     nbUsersStub.returns(Promise.resolve(12));
     nbFilesStub.returns(Promise.resolve(1231));
 
     const response = await request(app).get('/stats');
 
-    expect(response.status).to.equal(200);
-    expect(response.body).to.deep.equal({ users: 12, files: 1231 });
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ users: 12, files: 1231 });
   });
 });
