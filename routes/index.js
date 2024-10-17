54e13a51b04676532ea8002a3a758b2a05c39dc9
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-17 04:17:39 +0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,20 @@
-nkenfkr
+
+import express from 'express';
+import AppController from '../controllers/AppController.js';
+
+const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew)
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect)
+router.get('/users/me', UserController.getMe)
+router.post('/files', FilesController.postUpload);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
+
+
+
+export default router;
