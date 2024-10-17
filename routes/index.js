bfb960bfced528acf6ccc3ad5911847a6fd5d086
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-17 05:29:54 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
-
-
-
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
